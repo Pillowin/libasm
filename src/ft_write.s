8730ffa52b6c867c9866f84ee2f52d431229dc6b
WebForce3 <webforce3@c1r6p7.42quebec.com>
2022-07-05 15:26:23 -0400

    wip: ft_write

--- src/ft_write.s
+++ src/ft_write.s
@@ -1,9 +1,16 @@
+; ssize_t write(int fildes, const void *buf, size_t nbyte);
+;   RAX   write(    RDI   ,       RSI      ,      RDX    );
+
+; RAX: 1st return register
+; RDI: used to pass 1st argument to functions
+; RSI: used to pass 2nd argument to functions
+; RDX: used to pass 3rd argument to functions
+
+          global    _ft_write
+
           section   .text
 
-          global    _ft_write				; ssize_t write(int fildes, const void *buf, size_t nbyte);
-		                                    ;   RAX   write(		  ,                ,             );
-
-											; RAX -> Integer return values (similar to x86) are returned in RAX if 64 bits or less.
 _ft_write:
           mov       rax, 0x02000004         ; system call for write
           syscall                           ; invoke operating system to do the write
+		  ret
--- test/test_ft_write.cpp
+++ test/test_ft_write.cpp
@@ -1,15 +1,19 @@
 #include "Test.hpp"
+#include <unistd.h>
+#include <string>
 
-extern "C" ssize_t write(int fildes, const void *buf, size_t nbyte);
+extern "C" ssize_t ft_write(int fildes, const void *buf, size_t nbyte);
 
-static bool test_true(void)
+static bool test_hello_world(void)
 {
-	return (true);
+	std::string const str("Hello World!\n");
+	ssize_t ret = write(STDOUT_FILENO, str.c_str(), str.length()); // TODO: ft_write
+	ltest_assert_eq(ret, 13);
 }
 
 bool test_ft_write(void) {
 	Test t("ft_write");
 
-	t.registerTest("true", &test_true);
+	t.registerTest("stdout \"Hello World!\\n\"", &test_hello_world, "Hello World!\n");
 	return (t.run());
 }
