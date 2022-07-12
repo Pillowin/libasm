4631fc6f746fb54114caf8f1b2713965ae6913ab
Antoine GAUTIER <agautier@student.42quebec.com>
2022-07-12 11:28:02 -0400

    feat: add strlen test for different string size

--- test/test_ft_strlen.cpp
+++ test/test_ft_strlen.cpp
@@ -16,7 +16,31 @@ static bool test_empty(void) {
 	ltest_assert_eq(ret, 0);
 }
 
-static bool test_long_string(void) {
+static bool test_1_char(void) {
+	std::string const str("a");
+	size_t ret = ft_strlen(str.c_str());
+	ltest_assert_eq(ret, 1);
+}
+
+static bool test_15_char(void) {
+	std::string const str("0123456789abcde");
+	size_t ret = ft_strlen(str.c_str());
+	ltest_assert_eq(ret, 15);
+}
+
+static bool test_16_char(void) {
+	std::string const str("0123456789abcdef");
+	size_t ret = ft_strlen(str.c_str());
+	ltest_assert_eq(ret, 16);
+}
+
+static bool test_17_char(void) {
+	std::string const str("0123456789abcdefg");
+	size_t ret = ft_strlen(str.c_str());
+	ltest_assert_eq(ret, 17);
+}
+
+static bool test_2000_char(void) {
 	std::string const str(
 		"aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"
 		"aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"
@@ -56,6 +80,10 @@ bool test_ft_strlen(void) {
 
 	t.registerTest("\"Hello World!\\n\"", &test_hello_world);
 	t.registerTest("empty string", &test_empty);
-	t.registerTest("long string", &test_long_string);
+	t.registerTest("1 char string", &test_1_char);
+	t.registerTest("15 char string", &test_15_char);
+	t.registerTest("16 char string", &test_16_char);
+	t.registerTest("17 char string", &test_17_char);
+	t.registerTest("2000 char string", &test_2000_char);
 	return (t.run());
 }
