0390609a909a3d49e8348a561f9c6f166781cb8c
Antoine GAUTIER <agautier@student.42quebec.com>
2022-07-25 12:18:04 -0400

    clean(ft_write): clean tests

--- test/test_ft_write.cpp
+++ test/test_ft_write.cpp
@@ -28,14 +28,14 @@ static bool test_len_too_long(void) {
 static bool test_wrong_fd(void) {
 	std::string const str("Hello World!\n");
 	ssize_t ret = ft_write(42, str.c_str(), str.length());
-	if (errno != EBADF) // Bad file descriptor
+	if (ret != -1 || errno != EBADF) // Bad file descriptor
 		return (false);
 	ltest_assert_eq(ret, -1);
 }
 
 static bool test_null_str(void) {
 	ssize_t ret = ft_write(STDOUT_FILENO, NULL, 1);
-	if (errno != EFAULT) // Bad address
+	if (ret != -1 || errno != EFAULT) // Bad address
 		return (false);
 	ltest_assert_eq(ret, -1);
 }
