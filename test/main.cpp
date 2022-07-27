5b8c2aa19c6bd531f306c2910e2d4933506a4a48
Antoine GAUTIER <agautier@student.42quebec.com>
2022-07-27 13:15:19 -0400

    wip(ft_atoi_base): base checks ok and tests ok

--- test/main.cpp
+++ test/main.cpp
@@ -7,21 +7,24 @@ bool test_ft_strcpy(void);
 bool test_ft_strdup(void);
 bool test_ft_write(void);
 bool test_ft_read(void);
+bool test_ft_atoi_base(void);
 
 int main(void) {
 	bool passed = true;
 
-	if (!test_ft_strlen())
-		passed &= false;
-	if (!test_ft_strcmp())
-		passed &= false;
-	if (!test_ft_strcpy())
-		passed &= false;
-	if (!test_ft_strdup())
-		passed &= false;
-	if (!test_ft_write())
-		passed &= false;
-	if (!test_ft_read())
+	// if (!test_ft_strlen())
+	//	passed &= false;
+	// if (!test_ft_strcmp())
+	//	passed &= false;
+	// if (!test_ft_strcpy())
+	//	passed &= false;
+	// if (!test_ft_strdup())
+	//	passed &= false;
+	// if (!test_ft_write())
+	//	passed &= false;
+	// if (!test_ft_read())
+	//	passed &= false;
+	if (!test_ft_atoi_base())
 		passed &= false;
 	return (passed ? EXIT_SUCCESS : EXIT_FAILURE);
 }
